e8ebebbc6b3d86ade9f4054664032cb8af55e0cb
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-13 14:41:42 +0800

    Update Iris.py

--- Iris.py
+++ Iris.py
@@ -4,6 +4,7 @@ from sklearn.datasets import load_iris
 from sklearn import svm
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.manifold import TSNE
 
 iris = load_iris()
 
@@ -30,7 +31,6 @@ def Iris():
 def Iris_LSVC():
     sub_data1 = iris_df[iris_df.species != 2]
     sub_X1 = sub_data1.drop(['species', 'sepal_length', 'sepal_width'], axis=1)
-    sub_X1_1 = sub_data1.drop(['sepal_length', 'sepal_width'], axis=1)
     sub_Y1 = sub_data1.species
 
     clf1 = svm.LinearSVC()
@@ -38,27 +38,46 @@ def Iris_LSVC():
 
     # 获取分类结果
     prediction = clf1.predict(sub_X1)
+    # print(prediction)
 
     # 绘制分类超平面
     w = clf1.coef_
     b = clf1.intercept_
-
     x = np.linspace(1.5, 3.5, 100)
     y = (w[0, 0] * x + b[0]) / (-w[0, 1])
+
     plt.plot(x, y, 'r')
 
     c1 = sub_Y1 == 0
     c2 = sub_Y1 == 1
     colors = np.asarray([i for i in map(lambda a: 'yellowgreen' if a == 1 else 'steelblue', prediction)])
 
-    # print(c1)
-    # print(c2)
-
-    print(sub_X1_1)
-
-    # plt.scatter(sub_X1[c1, 0], sub_X1[c1, 1], c=colors[c1], s=60, alpha=0.5, marker='s')
-    # plt.scatter(sub_X1[c2, 0], sub_X1[c2, 1], c=colors[c2], s=60, alpha=0.5, marker='o')
-    # plt.scatter(sub_X1[:,0], sub_X1[:,1], c=colors[c2], s=60, alpha=0.5, marker='o')
+    plt.scatter(sub_X1.values[c1, 0], sub_X1.values[c1, 1], c=colors[c1], s=60, alpha=0.5, marker='s')
+    plt.scatter(sub_X1.values[c2, 0], sub_X1.values[c2, 1], c=colors[c2], s=60, alpha=0.5, marker='o')
 
     plt.grid()
+    plt.show()
+
+
+# 使用全部特征
+def Iris_LSVC_All():
+    X1 = iris_df.drop(['species'], axis=1)
+    Y1 = iris_df.species
+
+    clf2 = svm.LinearSVC()
+    clf2.fit(X1, Y1)
+    prediction = clf2.predict(X1)
+
+    X1_tSNE = TSNE(n_components=2).fit_transform(X1)
+
+    c1 = Y1 == 0
+    c2 = Y1 == 1
+    c3 = Y1 == 2
+
+    colors = ['steelblue', 'yellowgreen', 'purple']
+    dye = lambda colors, x: np.asarray([colors[int(i)] for i in x])
+
+    plt.scatter(X1_tSNE[c1, 0], X1_tSNE[c1, 1], c=dye(colors, prediction[c1]), s=60, alpha=0.5, marker='s')
+    plt.scatter(X1_tSNE[c2, 0], X1_tSNE[c2, 1], c=dye(colors, prediction[c2]), s=60, alpha=0.5, marker='o')
+    plt.scatter(X1_tSNE[c3, 0], X1_tSNE[c3, 1], c=dye(colors, prediction[c3]), s=60, alpha=0.5, marker='^')
     plt.show()
