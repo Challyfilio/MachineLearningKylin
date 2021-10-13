04af3055333ca59399264cb224e101e220163c2e
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-13 15:21:36 +0800

    Update Iris.py

--- Iris.py
+++ Iris.py
@@ -1,3 +1,4 @@
+#支持向量机：数据分类实践 鸢尾花
 import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris
@@ -13,7 +14,9 @@ iris_target = iris.target.reshape((150, 1))
 iris_data = np.hstack((iris.data, iris_target))
 iris_df = pd.DataFrame(iris_data)
 iris_df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
-print(iris_df.head())
+
+
+# print(iris_df.head())
 
 
 # 鸢尾花
@@ -87,6 +90,34 @@ def Iris_LSVC_All():
     # 置信分
     confidence = clf2.decision_function(X1[:5])
     print(confidence)
+
+    target_names = ['setosa', 'versicolor', 'virginica']
+    print(classification_report(Y1, prediction, target_names=target_names, digits=5))
+
+
+# 线性不可分
+def Iris_SVC():
+    X1 = iris_df.drop(['species'], axis=1)
+    Y1 = iris_df.species
+
+    clf3 = svm.SVC(kernel='rbf', gamma=20, decision_function_shape='ovr')
+    clf3.fit(X1, Y1)
+    prediction = clf3.predict(X1)
+
+    X1_tSNE = TSNE(n_components=2).fit_transform(X1)  # 降维数据
+
+    c1 = Y1 == 0
+    c2 = Y1 == 1
+    c3 = Y1 == 2
+
+    colors = ['steelblue', 'yellowgreen', 'purple']
+    dye = lambda colors, x: np.asarray([colors[int(i)] for i in x])
+
+    plt.scatter(X1_tSNE[c1, 0], X1_tSNE[c1, 1], c=dye(colors, prediction[c1]), s=60, alpha=0.5, marker='s')
+    plt.scatter(X1_tSNE[c2, 0], X1_tSNE[c2, 1], c=dye(colors, prediction[c2]), s=60, alpha=0.5, marker='o')
+    plt.scatter(X1_tSNE[c3, 0], X1_tSNE[c3, 1], c=dye(colors, prediction[c3]), s=60, alpha=0.5, marker='^')
+    plt.grid()
+    plt.show()
 
     target_names = ['setosa', 'versicolor', 'virginica']
     print(classification_report(Y1, prediction, target_names=target_names, digits=5))
