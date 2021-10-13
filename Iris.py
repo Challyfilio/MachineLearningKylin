e84ca0c08a8bf3bae0f4dcd8b7e514aee45ace41
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-13 09:37:25 +0800

    Update Iris.py

--- Iris.py
+++ Iris.py
@@ -30,6 +30,7 @@ def Iris():
 def Iris_LSVC():
     sub_data1 = iris_df[iris_df.species != 2]
     sub_X1 = sub_data1.drop(['species', 'sepal_length', 'sepal_width'], axis=1)
+    sub_X1_1 = sub_data1.drop(['sepal_length', 'sepal_width'], axis=1)
     sub_Y1 = sub_data1.species
 
     clf1 = svm.LinearSVC()
@@ -50,15 +51,14 @@ def Iris_LSVC():
     c2 = sub_Y1 == 1
     colors = np.asarray([i for i in map(lambda a: 'yellowgreen' if a == 1 else 'steelblue', prediction)])
 
-    print(sub_Y1 == 0)
-
     # print(c1)
     # print(c2)
 
-    print(sub_X1)
+    print(sub_X1_1)
 
     # plt.scatter(sub_X1[c1, 0], sub_X1[c1, 1], c=colors[c1], s=60, alpha=0.5, marker='s')
     # plt.scatter(sub_X1[c2, 0], sub_X1[c2, 1], c=colors[c2], s=60, alpha=0.5, marker='o')
+    # plt.scatter(sub_X1[:,0], sub_X1[:,1], c=colors[c2], s=60, alpha=0.5, marker='o')
 
     plt.grid()
     plt.show()
